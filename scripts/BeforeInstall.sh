76698eea155e5701a82ba1a6b5beddcf3300a2c6
Roman Kozulia <rk8318@rit.edu>
2020-11-10 13:15:21 -0500

    commented update/upgrade

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -2,7 +2,7 @@
 
 #update the system
 
-rm -rf /home/app/doorSensor
+#rm -rf /home/app/doorSensor
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
